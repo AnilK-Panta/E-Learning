ad8d49989c8616efafa207824afe5e14ab231bcf
Anil K. Panta <anilpanta2053@gmail.com>
2021-12-04 15:43:50 +0545

    hero banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./Components/Hero";
 import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ src/Components/Hero.js
@@ -0,0 +1,246 @@
+import React from "react";
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div
+      className="heroSection"
+      style={{ backgroundImage: "url(./image/heroImage.png)" }}
+    >
+      <div className="maxWidthHold heroSectionContent">
+        <div className="heroSectionTitle">
+          <h1>Better Future For Your Kids</h1>
+          <p>Let the child be the director, and the actor in their own pray</p>
+        </div>
+        <div className="heroCTA">
+          <span>Get Started</span>
+          <div className="CTAbtn">
+            <svg
+              width="8"
+              height="12"
+              viewBox="0 0 8 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4.93955 5.64641L1.00011 1.70697L1.707 1.00008L6.70689 5.99997L1.707 10.9999L1.00011 10.293L4.93955 6.35352L5.2931 5.99997L4.93955 5.64641Z"
+                fill="white"
+                stroke="white"
+                stroke-miterlimit="16"
+              />
+            </svg>
+          </div>
+        </div>
+        <svg
+          id="contentSvg"
+          width="551"
+          height="232"
+          viewBox="0 0 551 232"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="dataShow">
+            <g id="leftSection">
+              <g id="profileIconSection">
+                <g id="AccountIcon" filter="url(#filter0_d_12_41)">
+                  <rect
+                    x="6"
+                    y="3"
+                    width="52"
+                    height="52"
+                    rx="26"
+                    fill="white"
+                  />
+                  <g id="account">
+                    <path
+                      id="Vector"
+                      d="M32 19.6667C33.2377 19.6667 34.4247 20.1584 35.2998 21.0336C36.175 21.9088 36.6667 23.0957 36.6667 24.3334C36.6667 25.5711 36.175 26.7581 35.2998 27.6332C34.4247 28.5084 33.2377 29.0001 32 29.0001C30.7623 29.0001 29.5753 28.5084 28.7002 27.6332C27.825 26.7581 27.3333 25.5711 27.3333 24.3334C27.3333 23.0957 27.825 21.9088 28.7002 21.0336C29.5753 20.1584 30.7623 19.6667 32 19.6667V19.6667ZM32 31.3334C37.1567 31.3334 41.3333 33.4217 41.3333 36.0001V38.3334H22.6667V36.0001C22.6667 33.4217 26.8433 31.3334 32 31.3334Z"
+                      fill="#3DBD96"
+                    />
+                  </g>
+                </g>
+                <g id="accountContent">
+                  <text
+                    id="7.5k+"
+                    fill="#160041"
+                    space="preserve"
+                    style={{ whiteSpace: "pre" }}
+                    font-family="Poppins"
+                    font-size="40"
+                    font-weight="600"
+                    letter-spacing="-0.02em"
+                  >
+                    <tspan x="78" y="45">
+                      7.5k+
+                    </tspan>
+                  </text>
+                  <text
+                    id="Total active students taking gifted courses"
+                    fill="#160041"
+                    space="preserve"
+                    style={{ whiteSpace: "pre" }}
+                    font-family="Poppins"
+                    font-size="14"
+                    letter-spacing="-0.02em"
+                  >
+                    <tspan x="78" y="84.9">
+                      Total active students taking{" "}
+                    </tspan>
+                    <tspan x="78" y="106.9">
+                      gifted courses
+                    </tspan>
+                  </text>
+                </g>
+              </g>
+              <path
+                id="pointRight"
+                d="M268.001 184.932C268.291 184.305 268.017 183.562 267.39 183.273L257.177 178.556C256.55 178.266 255.808 178.54 255.518 179.167C255.229 179.793 255.502 180.536 256.129 180.826L265.207 185.018L261.014 194.097C260.725 194.724 260.998 195.466 261.625 195.756C262.252 196.045 262.995 195.772 263.284 195.145L268.001 184.932ZM204.194 155.275C202.996 155.631 202.996 155.632 202.996 155.633C202.996 155.633 202.997 155.634 202.997 155.635C202.998 155.638 202.999 155.641 203 155.644C203.002 155.652 203.005 155.662 203.009 155.675C203.017 155.701 203.028 155.738 203.044 155.786C203.074 155.881 203.118 156.019 203.178 156.197C203.297 156.552 203.476 157.065 203.721 157.708C204.21 158.993 204.962 160.8 206.019 162.909C208.129 167.121 211.468 172.567 216.382 177.453C221.301 182.344 227.812 186.686 236.241 188.639C244.674 190.593 254.929 190.134 267.298 185.581L266.434 183.235C254.447 187.648 244.69 188.03 236.805 186.203C228.916 184.375 222.804 180.313 218.144 175.68C213.479 171.041 210.284 165.842 208.254 161.789C207.241 159.766 206.521 158.036 206.057 156.818C205.825 156.208 205.657 155.728 205.548 155.402C205.493 155.239 205.454 155.115 205.428 155.034C205.415 154.993 205.406 154.963 205.4 154.944C205.397 154.934 205.395 154.927 205.394 154.923C205.393 154.921 205.393 154.92 205.392 154.92C205.392 154.919 205.392 154.919 205.392 154.919C205.392 154.919 205.393 154.92 204.194 155.275Z"
+                fill="#C9BDE0"
+              />
+            </g>
+            <g id="rightSection">
+              <path
+                id="pointLeft"
+                d="M287.012 13.8276C286.589 14.3731 286.688 15.1583 287.234 15.5815L296.122 22.4777C296.668 22.9009 297.453 22.8018 297.876 22.2564C298.299 21.7109 298.2 20.9257 297.655 20.5025L289.754 14.3725L295.884 6.47168C296.307 5.92624 296.208 5.14101 295.662 4.71783C295.117 4.29464 294.332 4.39375 293.909 4.93919L287.012 13.8276ZM342.5 57.0938C343.748 57.0174 343.748 57.0165 343.748 57.0155C343.748 57.0149 343.747 57.0137 343.747 57.0125C343.747 57.0102 343.747 57.0071 343.747 57.0033C343.746 56.9956 343.746 56.985 343.745 56.9714C343.743 56.9444 343.74 56.9057 343.736 56.8558C343.728 56.7559 343.716 56.6112 343.698 56.4247C343.662 56.0519 343.603 55.512 343.509 54.8306C343.321 53.4683 342.996 51.5379 342.442 49.2454C341.335 44.6667 339.308 38.608 335.621 32.7412C331.929 26.867 326.564 21.1709 318.791 17.3694C311.015 13.5663 300.92 11.7033 287.843 13.3537L288.157 15.8341C300.83 14.2345 310.422 16.059 317.693 19.6152C324.967 23.173 330.008 28.5082 333.504 34.0714C337.005 39.6421 338.947 45.4271 340.012 49.8328C340.543 52.0324 340.854 53.8793 341.032 55.1714C341.121 55.8172 341.176 56.3236 341.21 56.6653C341.226 56.8362 341.237 56.9658 341.244 57.051C341.247 57.0936 341.249 57.1251 341.251 57.1451C341.251 57.1551 341.252 57.1622 341.252 57.1664C341.252 57.1685 341.252 57.1699 341.252 57.1705C341.252 57.1708 341.252 57.1707 341.252 57.1709C341.252 57.1706 341.252 57.1701 342.5 57.0938Z"
+                fill="#C9BDE0"
+              />
+              <g id="chargeIconSection">
+                <g id="chargeIcon" filter="url(#filter1_d_12_41)">
+                  <rect
+                    x="288"
+                    y="121.094"
+                    width="52.5006"
+                    height="52.0014"
+                    rx="26.0007"
+                    fill="white"
+                  />
+                  <g id="charge Icon" clip-path="url(#clip0_12_41)">
+                    <g id="Group">
+                      <path
+                        id="Vector_2"
+                        d="M319.127 135.196C319.274 135.282 319.387 135.414 319.449 135.572C319.511 135.73 319.517 135.904 319.467 136.066L316.766 144.844H321.75C321.897 144.844 322.04 144.887 322.162 144.968C322.285 145.048 322.381 145.163 322.439 145.297C322.497 145.432 322.515 145.58 322.489 145.724C322.464 145.869 322.397 146.002 322.296 146.109L310.296 158.859C310.18 158.982 310.026 159.063 309.858 159.087C309.69 159.112 309.519 159.079 309.373 158.993C309.226 158.908 309.113 158.776 309.051 158.618C308.99 158.46 308.983 158.286 309.033 158.124L311.735 149.344H306.75C306.604 149.344 306.461 149.302 306.338 149.221C306.216 149.141 306.12 149.026 306.061 148.892C306.003 148.757 305.986 148.609 306.011 148.464C306.037 148.32 306.104 148.186 306.204 148.08L318.204 135.33C318.32 135.207 318.475 135.126 318.642 135.102C318.81 135.077 318.98 135.11 319.127 135.195V135.196Z"
+                        fill="#3DBD96"
+                      />
+                    </g>
+                  </g>
+                </g>
+                <g id="chargeContent">
+                  <text
+                    id="50k+"
+                    fill="#160041"
+                    space="preserve"
+                    style={{ whiteSpace: "pre" }}
+                    font-family="Poppins"
+                    font-size="40"
+                    font-weight="600"
+                    letter-spacing="-0.02em"
+                  >
+                    <tspan x="361.501" y="163.094">
+                      50k+
+                    </tspan>
+                  </text>
+                  <text
+                    id="Available field programs and increasing"
+                    fill="#160041"
+                    space="preserve"
+                    style={{ whiteSpace: "pre" }}
+                    font-family="Poppins"
+                    font-size="14"
+                    letter-spacing="-0.02em"
+                  >
+                    <tspan x="361.501" y="202.994">
+                      Available field programs{" "}
+                    </tspan>
+                    <tspan x="361.501" y="224.994">
+                      and increasing
+                    </tspan>
+                  </text>
+                </g>
+              </g>
+            </g>
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_12_41"
+              x="0"
+              y="0"
+              width="64"
+              height="64"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy="3" />
+              <feGaussianBlur stdDeviation="3" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_12_41"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_12_41"
+                result="shape"
+              />
+            </filter>
+            <filter
+              id="filter1_d_12_41"
+              x="282"
+              y="118.094"
+              width="64.5006"
+              height="64.0015"
+              filterUnits="userSpaceOnUse"
+              color-interpolation-filters="sRGB"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy="3" />
+              <feGaussianBlur stdDeviation="3" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_12_41"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_12_41"
+                result="shape"
+              />
+            </filter>
+            <clipPath id="clip0_12_41">
+              <rect
+                width="24.5006"
+                height="24.0014"
+                fill="white"
+                transform="translate(302 135.094)"
+              />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  );
+}
