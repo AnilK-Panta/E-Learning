680469eab3b40f2489dd21412c8188b8583e8522
Anil K. Panta <anilpanta2053@gmail.com>
2021-12-25 13:31:06 +0545

    mentor card changed

--- src/Components/MentorCard.js
+++ src/Components/MentorCard.js
@@ -1,22 +1,33 @@
+import { Link } from "react-router-dom";
+
 export default function MentorCard() {
   return (
     <div
       style={{
         width: "250px",
+        backgroundColor: "#FFFFFF",
+        borderRadius: "15px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "1em",
+        padding: "1em 0",
       }}
     >
       <img
         src="./image/mentor1.png"
         alt="mentor"
         style={{
-          width: "250px",
-          height: "250px",
-          borderRadius: "10px",
+          width: "130px",
+          height: "130px",
+          borderRadius: "40px",
         }}
       />
       <div
         style={{
-          marginTop: ".8em",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
         <span
@@ -36,6 +47,25 @@ export default function MentorCard() {
           Founder & Mentor
         </p>
       </div>
+      <Link
+        to="/"
+        style={{
+          textDecoration: "none",
+        }}
+      >
+        <span
+          className="mentorCardBtn"
+          style={{
+            color: "#9C4DF4",
+            padding: ".5em 1.5em",
+            border: "1px solid #9C4DF4",
+            borderRadius: "5px",
+            fontWeight: "500",
+          }}
+        >
+          Visit Profile
+        </span>
+      </Link>
     </div>
   );
 }
